7bf05842e7275028a9d54b5daa496778f82d6c65
Agung Mahadana <100138244+agungmahadana@users.noreply.github.com>
2022-08-16 22:30:59 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// typing animation
+var typed = new Typed(".typing", {
+  strings: ["Student at Udayana University", "Amateur Designer"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
+
+// active navbar
+// const nav = document.querySelector(".nav");
+// const aa = document.querySelectorAll(".menu");
+// nav.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   if (e.target.classList.contains("menu")) {
+//     aa.forEach(function (a) {
+//       a.className = "menu";
+//     });
+//     e.target.classList.add("active");
+//   }
+// });
